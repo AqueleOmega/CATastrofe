cf560fa00ea9da3054a2dc31a2f519b8df969770
Giovanni Florio Sannazzaro <giovannifloriosannazzarro@gmail.com>
2026-08-12 21:22:48 -0300

    movimentação
    nada demais, terceira vez fazendo isso

---
+++ My project (1)/Assets/Scripts/Movimentação.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Movimentação : MonoBehaviour
+{
+    Vector2 input;
+    public Rigidbody2D rb;
+    public float velocidade;
+    
+    void Start()
+    {
+        
+    }
+
+    public void Input(InputAction.CallbackContext context)
+    {
+        input = context.ReadValue<Vector2>();
+    }
+
+    void Update()
+    {
+
+        rb.linearVelocity = input * velocidade;
+    }
+}

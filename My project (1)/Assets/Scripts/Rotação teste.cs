950b1d23f8a66c40d0226b184bf550f20092a920
Giovanni Florio Sannazzaro <giovannifloriosannazzarro@gmail.com>
2026-08-19 16:34:05 -0300

    fim da aula/// rotação1 feita

--- My project (1)/Assets/Scripts/Rotação teste.cs
+++ My project (1)/Assets/Scripts/Rotação teste.cs
@@ -9,24 +9,31 @@ public class Rotaçãoteste : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Quaternion myRotation = Quaternion.identity;
+        myRotation.eulerAngles 
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Debug.Log(transform1);
-        // Debug.Log(transform2);
         vector_dist = transform1.position - transform2.position;
+        dist_norm = vector_dist.normalized;
+        float m_Angle = Vector2.SignedAngle(Vector2.right, dist_norm); 
+        myRotation.eulerAngles = new Vector3 (0,0,m_Angle)
+        transform1.rotation.z = myRotation;
+
+        Debug.Log(m_Angle);
         Debug.Log(vector_dist);
-        Vector2 dist_norm = vector_dist.normalized;
         Debug.Log(dist_norm);
     }
+/*
     private void OnDrawGizmos(){
+        vector_dist = transform1.position - transform2.position;
         Vector2 dist_norm = vector_dist.normalized;
         Gizmos.color = Color.yellow;
         //Gizmos.DrawLine(transform1.position, transform2.position);
         //Gizmos.color = Color.red;
-        Gizmos.DrawLine(transform1.position, dist_norm);
+        Gizmos.DrawLine(Vector2.zero, dist_norm);
     }
+    */
 }
